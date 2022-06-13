1f71ff163e62f81af244ec6db4ca63c9ee606d0d
insolor <insolor@gmail.com>
2022-06-13 18:41:51 +0300

    Generate random number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Угадай число!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Введите ваш вариант: ");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Нужно ввести число!");
+
+    println!("Вы ввели: {guess}");
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Слишком маленькое!"),
+        Ordering::Greater => println!("Слишком большое!"),
+        Ordering::Equal => println!("Вы угадали!"),
+    }
 }
