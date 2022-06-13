c7fa3a6d260cb29786defab7073d9e30d71ffffb
insolor <insolor@gmail.com>
2022-06-13 19:02:31 +0300

    Handle invalid entry

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,12 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-    
-        let guess: u32 = guess.trim().parse().expect("Нужно ввести число!");
-    
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
         println!("Вы ввели: {guess}");
 
         match guess.cmp(&secret_number) {
