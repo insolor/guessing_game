c4902cc4b46dd7787bd1b32ef356ac156cb13d44
insolor <insolor@gmail.com>
2022-06-13 18:52:10 +0300

    Guessing in a loop

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,26 @@ fn main() {
     
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Введите ваш вариант: ");
+    loop {
+        println!("Введите ваш вариант: ");
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Нужно ввести число!");
-
-    println!("Вы ввели: {guess}");
+        let mut guess = String::new();
     
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Слишком маленькое!"),
-        Ordering::Greater => println!("Слишком большое!"),
-        Ordering::Equal => println!("Вы угадали!"),
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = guess.trim().parse().expect("Нужно ввести число!");
+    
+        println!("Вы ввели: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Слишком маленькое!"),
+            Ordering::Greater => println!("Слишком большое!"),
+            Ordering::Equal => {
+                println!("Вы угадали!");
+                break;
+            }
+        }
     }
 }
